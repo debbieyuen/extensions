fc06dcb27cf3099650ef30d6ecb5f849ee79ab7f
Debbie Yuen <deborahyuen2016@berkeley.edu>
2023-09-09 14:15:59 -0700

    get fetch requests

--- wds-search-blog-example/extension.js
+++ wds-search-blog-example/extension.js
@@ -2,6 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const axios = require('axios');
+// const xmlParser = require('fast-xml-parser');
+
+const {XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,8 +13,20 @@ const axios = require('axios');
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-	const res = await axios.get("https://blog.webdevsimplified.com/rss.xml")
-	console.log(res.data)
+	const res = await axios.get("https://blog.webdevsimplified.com/rss.xml");
+	// console.log(xmlParser.parse(res.data))
+	const parser = new XMLParser();
+	const articles = parser.parse(res.data).rss.channel.item.map
+	(article => {
+		return {
+			label: article.title,
+			detail: article.description,
+			link: article.link,
+		}
+	});
+	console.log(articles);
+	// console.log(parser.parse(res.data));
+	// console.log(res.data)
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -20,9 +35,15 @@ async function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('wds-search-blog-example.searchWdsBlogExample', function () {
+	let disposable = vscode.commands.registerCommand('wds-search-blog-example.helloWorld', 
+	async function () {
+		const article = await vscode.window.showQuickPick(articles, {
+			matchOnDetail: true,
+		})
 		// The code you place here will be executed every time your command is executed
-
+		console.log(article);
+		if (article == null) return;
+		vscode.env.openExternal(article.link);
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from WDS Search Blog Example!');
 	});
